62790ff5d421dc51695df34536a116af2a3e44ab
nurdin73 <nurdin.reverse73@gmail.com>
2019-12-21 17:07:41 +0700

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-const port = 5000;
+const port = process.env.PORT || 5000;
 const categoriesController = require("./controllers/categories");
 const articlesController = require("./controllers/articles");
 const authController = require("./controllers/auth");
